7e4f357f123ff09d33c110ba452e6b7ce62a17af
marckr02 <marco.rios@epn.edu.ec>
2025-08-04 23:46:41 -0500

    Add muted attribute to videos for silent autoplay

--- components/GallerySection.tsx
+++ components/GallerySection.tsx
@@ -35,7 +35,7 @@ const Modal: React.FC<{
                        {item.type === 'image' ? (
                             <img src={item.src} alt={item.title} className="w-full h-full object-contain" />
                         ) : (
-                            <video src={item.src} controls autoPlay className="w-full h-full object-contain">
+                            <video src={item.src} controls autoPlay muted className="w-full h-full object-contain">
                                 Your browser does not support the video tag.
                             </video>
                         )}
